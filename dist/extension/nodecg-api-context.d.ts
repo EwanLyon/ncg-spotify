d73f35b7fb493919977081f4e8103376bd730203
Clubwho <clubwhomc@gmail.com>
2019-03-01 18:39:33 +1100

    Move api context

---
+++ dist/extension/nodecg-api-context.d.ts
@@ -0,0 +1,10 @@
+import { NodeCG } from '../types/nodecg';
+/** Gets the current NodeCG context
+ * @return {NodeCG} Current NodeCG context
+ */
+export declare function get(): NodeCG;
+/** Sets the current NodeCG context
+ *  @param {NodeCG} ctx NodeCG context
+ */
+export declare function set(ctx: NodeCG): void;
+//# sourceMappingURL=nodecg-api-context.d.ts.map
--- dist/extension/util/nodecg-api-context.d.ts
+++
@@ -1,4 +0,0 @@
-import { NodeCG } from '../../types/nodecg';
-export declare function get(): NodeCG;
-export declare function set(ctx: NodeCG): void;
-//# sourceMappingURL=nodecg-api-context.d.ts.map
